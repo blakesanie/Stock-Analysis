b3eafb878e35aee2068d2f68f29568fb15fe7305
Blake Sanie <blake@sanie.com>
2019-07-13 16:19:53 -0700

    calculating fit equation and annual return here

--- regression.py
+++ regression.py
@@ -1,18 +1,5 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
-# from scipy.optimize import curve_fit
-
-# x = np.arange(1,7).reshape((-1, 1))
-# y = np.log([1, 1.5, 3, 6, 10, 15])
-#
-# model = LinearRegression()
-#
-# model.fit(x, y)
-#
-# r_sq = model.score(x, y)
-# a = np.exp(model.intercept_)
-# b = np.exp(model.coef_)[0]
-# print("y ~ {}*{}^x".format(a,b))
 
 def exponentialRegression(closing):
     x = np.arange(1,len(closing) + 1).reshape((-1, 1))
@@ -20,4 +7,15 @@ def exponentialRegression(closing):
     y_ln = np.log(y_normalized)
     model = LinearRegression()
     model.fit(x, y_ln)
-    return (np.exp(model.intercept_), np.power(np.exp(model.coef_)[0], 365.25), model.score(x, y_ln))
+    scalar = np.exp(model.intercept_) * closing[0]
+    base = np.exp(model.coef_)[0]
+    annualReturn = (np.power(np.exp(model.coef_)[0], 365.25) - 1) * 100
+    equation = "y = {} * {}^x + {}".format(scalar, base, closing[0])
+    rSquared = model.score(x, y_ln)
+    return {
+        "scalar": scalar,
+        "base": base,
+        "annualReturn": annualReturn,
+        "equation": equation,
+        "rSquared": rSquared
+    }
