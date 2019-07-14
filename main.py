be48d03d0e38a6e95c1cfac70db5e3f7325f4fa1
Blake Sanie <blake@sanie.com>
2019-07-13 17:47:15 -0700

    enhanced error handling, also writing to json file

--- main.py
+++ main.py
@@ -2,17 +2,30 @@ from Stock import Stock
 from finsymbols import symbols
 import csv
 import os
+import json
 
 companies = symbols.get_sp500_symbols()[0:2]
 
-with open('SP500StockData.csv', 'w') as output:
-    writer = csv.writer(output)
+stockDicts = []
+
+with open('SP500StockData.csv', 'w') as csvOut:
+    writer = csv.writer(csvOut)
     writer.writerow(("Symbol","Company","Sector","Industry","Exp. Regression Equation","Annual Return (%)","R^2","First Date","Last Date"))
     for i, company in enumerate(companies):
         stock = Stock(company)
-        stock.findClosingPrices()
+        try:
+            stock.findClosingPrices()
+        except Exception as e:
+            print(e)
+            break
         stock.findRegressionValues()
         writer.writerow(stock.getRow())
+        stockDicts.append(stock.getDict())
 
-output.close()
-os.system("open SP500StockData.csv")
+csvOut.close()
+with open('data.json', 'w') as jsonOut:
+    data = json.dumps(stockDicts, indent=4, sort_keys=True)
+    json.dump(data, jsonOut)
+
+jsonOut.close()
+# os.system("open SP500StockData.csv")
