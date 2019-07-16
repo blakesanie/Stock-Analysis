5ecd09d6f22776353fb1ab1de3f7ac922957d349
Blake Sanie <blake@sanie.com>
2019-07-16 15:53:32 -0700

    better exception handling, cleaned up

--- main.py
+++ main.py
@@ -4,28 +4,30 @@ import csv
 import os
 import json
 
-companies = symbols.get_sp500_symbols()[0:2]
+companies = symbols.get_sp500_symbols()#[0:5]
+print(len(companies))
 
 stockDicts = []
 
 with open('SP500StockData.csv', 'w') as csvOut:
     writer = csv.writer(csvOut)
-    writer.writerow(("Symbol","Company","Sector","Industry","Exp. Regression Equation","Annual Return (%)","R^2","First Date","Last Date"))
+    writer.writerow(("Symbol","Company","Sector","Industry","avg. ROI (%) - Max","avg. ROI (%) - 20 Years","avg. ROI (%) - 10 Years","avg. ROI (%) - 5 Years","avg. ROI (%) - 1 Year","avg. ROI (%) - 6 Months","R^2 - Max", "R^2 - 20 Years", "R^2 - 10 Years", "R^2 - 5 Years", "R^2 - 1 Year", "R^2 - 6 Months", "First Date", "Last Date"))
     for i, company in enumerate(companies):
         stock = Stock(company)
         try:
             stock.findClosingPrices()
         except Exception as e:
-            print(e)
-            break
+            print(str(e))
+            if not str(e) == "no more api calls":
+                continue
+            else:
+                break
         stock.findRegressionValues()
         writer.writerow(stock.getRow())
         stockDicts.append(stock.getDict())
 
 csvOut.close()
 with open('data.json', 'w') as jsonOut:
-    data = json.dumps(stockDicts, indent=4, sort_keys=True)
-    json.dump(data, jsonOut)
+    json.dump(stockDicts, jsonOut)
 
 jsonOut.close()
-# os.system("open SP500StockData.csv")
